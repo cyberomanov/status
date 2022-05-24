55ed577d63a1ea2d604c2009e33e99a70a7ddea2
cyberomanov <41644451+cyberomanov@users.noreply.github.com>
2022-05-24 09:31:44 +0400

    Update cosmos.sh

--- tendermint/node_status/cosmos.sh
+++ tendermint/node_status/cosmos.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function __sendFunc() {
+    # print 'TEXT' into 'cosmos.log' for the sake of history
+    echo -e ${TEXT}
+
+    # add new text to the 'MESSAGE', which will be sent as 'log' or 'alarm'
+    # if 'SEND' == 1, it becomes 'alarm', otherwise it's 'log'
+    MESSAGE=${MESSAGE}'<code>'${TEXT}'</code>\n'
+}
+
 function __getLastChainBlockFunc() {
 
     # get the last explorer block
@@ -16,14 +25,73 @@ function __getLastChainBlockFunc() {
     echo ${LATEST_CHAIN_BLOCK}
 }
 
+function __getSignedAndMissedBlocksFunc() {
+
+    # get slashing params
+    SLASHING=$(${COSMOS} q slashing params -o json --node ${NODE} --home ${NODE_HOME})
+    WINDOW=$(echo $SLASHING | jq ".signed_blocks_window" | tr -d '"')
+    MIN_SIGNED=$(echo $SLASHING | jq ".min_signed_per_window" | tr -d '"')
+
+    JAILED_AFTER=$(echo ${WINDOW}*${MIN_SIGNED} | bc -l | grep -oE "[0-9]*" | awk 'NR==1 {print; exit}')
+    MISSED_BLOCKS_FOR_ALARM=$(echo ${JAILED_AFTER}/10 | bc -l | grep -oE "[0-9]*" | awk 'NR==1 {print; exit}')
+
+    # get some info about node
+    NODE_STATUS_TOTAL=$(curl -s localhost:${PORT}/status)
+
+    VALIDATOR_ADDRESS=$(echo $NODE_STATUS_TOTAL | jq .result.validator_info.address | tr -d '"')
+    LATEST_BLOCK_HEIGHT=$(echo $NODE_STATUS_TOTAL | jq .result.sync_info.latest_block_height | tr -d '"')
+    LATEST_BLOCK_TIME=$(echo $NODE_STATUS_TOTAL | jq .result.sync_info.latest_block_time | tr -d '"')
+    TIME=$(date --iso-8601=ns -u)
+
+    PROPOSED=0
+    SIGNED=0
+    MISSED=0
+    MAX_ROW=0
+
+    LOOKBEHIND_BLOCKS=100
+
+    START_BLOCK=$(($LATEST_BLOCK_HEIGHT-$LOOKBEHIND_BLOCKS+1))
+    for (( BLOCK = $START_BLOCK; BLOCK <= $LATEST_BLOCK_HEIGHT; BLOCK++ ))
+    do
+        # check for validator signature
+        SIGNATURE=$(curl -s localhost:${PORT}/block?height=${BLOCK} | jq .result.block.last_commit.signatures[].validator_address | tr -d '"' | grep ${VALIDATOR_ADDRESS})
+
+        # if signature exists > signed + 1
+        if [[ ${SIGNATURE} != "" ]]; then
+            MISSED_IN_A_ROW=0
+            ((SIGNED=SIGNED+1))
+
+        # if signature does not exist > missed + 1
+        else
+            ((MISSED_IN_A_ROW=MISSED_IN_A_ROW+1))
+            ((MISSED=MISSED+1))
+            if (( ${MISSED_IN_A_ROW} > ${MAX_ROW} )); then MAX_ROW=${MISSED_IN_A_ROW}; fi
+        fi
+    done
+
+    # if missed more than 10% of allowed missed blocks before jail > alarm
+    if (( ${MISSED} > ${MISSED_BLOCKS_FOR_ALARM}))
+    then
+        SEND=1
+        TEXT="_missed > ${MISSED}/100 last blocks.\n_train >> ${MAX_ROW} missed in a row.\n_jailed > after ${JAILED_AFTER} missed blocks."
+    elif (( ${MISSED} > 0 ))
+    then
+        TEXT="missed >> ${MISSED} blocks.\ntrain >>> ${MAX_ROW} missed in a row."
+    else
+        TEXT="missed >> ${MISSED} blocks."
+    fi
+    __sendFunc
+}
+
+
 function nodeStatusFunc() {
 
-   MESSAGE="<b>${PROJECT} | ${MONIKER}</b>\n\n"
-   echo -e "${PROJECT} | ${MONIKER}\n"
+   MESSAGE="<b>${PROJECT} ⠀|⠀ ${MONIKER}</b>\n\n"
+   echo -e "${PROJECT}  |  ${MONIKER}\n"
 
    # if 'SEND' become '1' > alarm will be sent
    SEND=0
-   NODE_STATUS=$(${COSMOS} status 2>&1 --node ${NODE})
+   NODE_STATUS=$(${COSMOS} status 2>&1 --node ${NODE} --home ${NODE_HOME})
 
    # if 'NODE_STATUS' response contains 'connection refused' > instant alarm
    if [[ $NODE_STATUS != *"connection refused"* ]]
@@ -40,21 +108,20 @@ function nodeStatusFunc() {
        then
 
            # if we are in the past more than 10 block > alarm
-           if (( ${LATEST_CHAIN_BLOCK}-10 > ${LATEST_NODE_BLOCK} )); then SEND=1; fi
-           TEXT="sync >>> ${LATEST_CHAIN_BLOCK}/${LATEST_NODE_BLOCK}."
+           if (( ${LATEST_CHAIN_BLOCK}-10 > ${LATEST_NODE_BLOCK} ))
+           then
+               SEND=1
+               TEXT="_exp/me > ${LATEST_CHAIN_BLOCK}/${LATEST_NODE_BLOCK}."
+           else
+               TEXT="exp/me >> ${LATEST_CHAIN_BLOCK}/${LATEST_NODE_BLOCK}."
+           fi
        else
-           TEXT="sync >>> 0/${LATEST_NODE_BLOCK}."
+           TEXT="exp/me >> 0/${LATEST_NODE_BLOCK}."
        fi
-
-       # print 'TEXT' into 'cosmos.log' for the sake of history
-       echo ${TEXT}
-
-       # add new text to the 'MESSAGE', which will be sent as 'log' or 'alarm'
-       # if 'SEND' == 1, it becomes 'alarm', otherwise it's 'log'
-       MESSAGE=${MESSAGE}'<code>'${TEXT}'</code>\n'
+       __sendFunc
 
        # get validator info
-       VALIDATOR_INFO=$(${COSMOS} query staking validator ${VALIDATOR_ADDRESS} --node $NODE --output json)
+       VALIDATOR_INFO=$(${COSMOS} query staking validator ${VALIDATOR_ADDRESS} --node $NODE --output json --home ${NODE_HOME})
        BOND_STATUS=$(echo ${VALIDATOR_INFO} | jq .'status' | tr -d '"')
 
        # if 'BOND_STATUS' is different than 'BOND_STATUS_BONDED' > alarm
@@ -67,41 +134,47 @@ function nodeStatusFunc() {
            JAILED_STATUS=$(echo ${VALIDATOR_INFO} | jq .'jailed')
            if [[ "${JAILED_STATUS}" == "true" ]]
            then
-               TEXT="jailed > ${JAILED_STATUS}."
+               TEXT="jailed >> ${JAILED_STATUS}."
            else
-               TEXT="active > false."
+               TEXT="active >> false."
            fi
-           echo ${TEXT}
-           MESSAGE=${MESSAGE}'<code>'${TEXT}'</code>\n'
+           __sendFunc
 
        # if 'BOND_STATUS' is 'BOND_STATUS_BONDED' > continue
        else
 
            # get local explorer snapshot and request some info about our validator
-           EXPLORER=$(${COSMOS} q staking validators --node $NODE -o json --limit=1000)
+           EXPLORER=$(${COSMOS} q staking validators --node $NODE --output json --home ${NODE_HOME} --limit=1000)
            VALIDATORS_COUNT=$(echo ${EXPLORER} | jq '.validators[] | select(.status=="BOND_STATUS_BONDED")' | jq -r '.tokens' | sort -gr | wc -l)
            VALIDATOR_STRING=$(echo ${EXPLORER} | jq '.validators[] | select(.status=="BOND_STATUS_BONDED")' | jq -r '.tokens + " " + .description.moniker' | sort -gr | nl | grep -F ${MONIKER})
-           VALIDATOR_PLACE=$(echo ${VALIDATOR_STRING} | awk '{print $1}')
-           MAX_VALIDATOR_COUNT=$(${COSMOS} q staking params --node ${NODE} -o json | jq ."max_validators")
+           VALIDATOR_POSITION=$(echo ${VALIDATOR_STRING} | awk '{print $1}')
+           ACTIVE_VALIDATOR_SET=$(${COSMOS} q staking params --node ${NODE} --output json --home ${NODE_HOME} | jq ."max_validators")
 
-           # validator place in the set
-           TEXT="place >> ${VALIDATOR_PLACE}/${MAX_VALIDATOR_COUNT}."
-           echo ${TEXT}
-           MESSAGE=${MESSAGE}'<code>'${TEXT}'</code>\n'
+           # alarm if validator is close to become inactive
+           SAFE_VALIDATOR_PLACE=$(echo ${ACTIVE_VALIDATOR_SET} - ${POSITION_ALARM} | bc -l)
+           
+           if (( ${VALIDATOR_POSITION} > ${SAFE_VALIDATOR_PLACE} ))
+           then
+               SEND=1
+               TEXT="_place >> ${VALIDATOR_POSITION}/${ACTIVE_VALIDATOR_SET}."
+           else
+               TEXT="place >>> ${VALIDATOR_POSITION}/${ACTIVE_VALIDATOR_SET}."
+           fi
+           __sendFunc
 
            # validator active stake
            VALIDATOR_STAKE=$(echo ${VALIDATOR_STRING} | awk '{print $2}')
-           TEXT="stake >> $(echo "scale=2;${VALIDATOR_STAKE}/${DENOM}" | bc) ${TOKEN}."
-           echo ${TEXT}
-           MESSAGE=${MESSAGE}'<code>'${TEXT}'</code>\n'
+           TEXT="stake >>> $(echo "scale=2;${VALIDATOR_STAKE}/${DENOM}" | bc) ${TOKEN}."
+           __sendFunc
        fi
+       __getSignedAndMissedBlocksFunc
     else
 
        # if connection is refused > alarm
-       TEXT="connection is refused."
-       echo ${TEXT}
-       MESSAGE=${MESSAGE}'<code>'${TEXT}'</code>'
        SEND=1
+       TEXT="connection is refused."
+       __sendFunc
+
     fi
 
     # if 'SEND' == 1 > send 'MESSAGE' into 'alarm telegram channel'
@@ -135,7 +208,9 @@ do
         . $CONFIG
         # get '--node' and '--chain' value
         NODE=$(cat ${CONFIG}/config.toml | grep -oPm1 "(?<=^laddr = \")([^%]+)(?=\")")
+        NODE_HOME=$(echo $CONFIG | rev | cut -c 8- | rev)
         CHAIN=$(cat ${CONFIG}/genesis.json | jq .chain_id | sed -E 's/.*"([^"]+)".*/\1/')
+        PORT=$(echo ${NODE} | awk 'NR==1 {print; exit}' | grep -o ":[0-9]*" | awk 'NR==2 {print; exit}' | cut -c 2-)
 
         # print the current time
         echo -e " "
