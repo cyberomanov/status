03d3adde66208186165ef78c1ac602530998c38e
cyberomanov <41644451+cyberomanov@users.noreply.github.com>
2022-07-03 08:29:21 +0400

    Update nym.sh

--- nym/nym.sh
+++ nym/nym.sh
@@ -19,63 +19,75 @@ function nodeStatusFunc() {
     echo -e "${PROJECT} | ${MONIKER}\n"
     SEND=0
 
-    # get uptime
-    UPTIME_TOTAL=$(curl -s ${CURL}${IDENTITY}"/uptime")
-    LAST_HOUR=$(echo ${UPTIME_TOTAL} | jq ".last_hour" | tr -d '"')
-    LAST_DAY=$(echo ${UPTIME_TOTAL} | jq ".last_day" | tr -d '"')
-
-    # if 'last hour uptime' is low > alarm
-    if (( ${LAST_HOUR} < ${UPTIME} ))
+    RESPONSE=$(curl -s ${CURL}${IDENTITY})
+    if [[ ${RESPONSE} != *"error"* ]]
     then
+
+        # get uptime
+        UPTIME_TOTAL=$(curl -s ${CURL}${IDENTITY}"/uptime")
+        LAST_HOUR=$(echo ${UPTIME_TOTAL} | jq ".last_hour" | tr -d '"')
+        LAST_DAY=$(echo ${UPTIME_TOTAL} | jq ".last_day" | tr -d '"')
+
+        # if 'last hour uptime' is low > alarm
+        if (( ${LAST_HOUR} < ${UPTIME} ))
+        then
+            SEND=1
+            TEXT="_hour/day > $LAST_HOUR%/$LAST_DAY%."
+            __sendFunc
+        fi
+
+        # get info about node status
+        TOTAL_INFO=$(curl -s ${CURL}${IDENTITY})
+
+        # if node is outdated > alarm
+        OUTDATED=$(echo ${TOTAL_INFO} | jq ".outdated" | tr -d '"')
+        if [[ "${OUTDATED}" == "true" ]]
+        then
+            SEND=1
+            VERSION=$(echo ${TOTAL_INFO} | jq ".mixnode.mix_node.version" | tr -d '"')
+            TEXT="_version >> ${VERSION}.\n_outdated > ${OUTDATED}."
+            __sendFunc
+        fi
+
+        # if status is not active > alarm
+        STATUS=$(echo ${TOTAL_INFO} | jq ".mixnode.status" | tr -d '"')
+        if [[ "${STATUS}" != "active" ]]
+        then
+            SEND=1
+            TEXT="_status >>> ${STATUS}."
+            __sendFunc
+        fi
+
+        # get info about stake
+        DELEGATION=$(echo $(echo "scale=2;$(echo ${TOTAL_INFO} | jq ".mixnode.total_delegation.amount" | tr -d '"')/${DENOM}" | bc))
+        SELF_STAKE=$(echo $(echo "scale=2;$(echo ${TOTAL_INFO} | jq ".mixnode.pledge_amount.amount" | tr -d '"')/${DENOM}" | bc))
+        TOTAL_STAKE=$(echo ${SELF_STAKE} + ${DELEGATION} | bc -l)
+        TEXT="stake >>>>> ${TOTAL_STAKE} ${TOKEN}."
+        __sendFunc
+
+        # get info about rewards
+        TOTAL_REWARDS=$(curl -s ${CURL}${IDENTITY}"/estimated_reward")
+
+        # get info about estimated rewards
+        ESTIMATED_REWARDS_H=$(echo $(echo "scale=2;$(echo ${TOTAL_REWARDS} | jq ".estimated_operator_reward" | tr -d '"')/${DENOM}" | bc))
+        ESTIMATED_REWARDS_M=$(echo ${ESTIMATED_REWARDS_H}*720 | bc -l)
+        if (( $(bc <<< "${ESTIMATED_REWARDS_H} < 1") )); then ESTIMATED_REWARDS_H="0${ESTIMATED_REWARDS_H}"; fi
+        if (( $(bc <<< "${ESTIMATED_REWARDS_M} < 1") )); then ESTIMATED_REWARDS_M="0${ESTIMATED_REWARDS_M}"; fi
+        TEXT="salary >>>> ${ESTIMATED_REWARDS_M} per month."
+        __sendFunc
+
+        # get rewards
+        DELEGATOR=$(echo ${TOTAL_INFO} | jq ".address" | tr -d '"')
+        DELEGATOR_CLAIMABLE=$(curl -sk https://mixnet.api.explorers.guru/api/accounts/${DELEGATOR}/balance | jq ".claimable.amount" | tr -d '"')
+        UNPAID=$(echo $(echo "scale=2;${DELEGATOR_CLAIMABLE}/${DENOM}" | bc))
+        if (( $(bc <<< "${UNPAID} < 1") )); then UNPAID="0${UNPAID}"; fi
+        TEXT="unpaid >>>> ${UNPAID} ${TOKEN}."
+        __sendFunc
+    else
         SEND=1
-        TEXT="_hour/day > $LAST_HOUR%/$LAST_DAY%."
+        TEXT="_explorer is down."
         __sendFunc
     fi
-
-    # get info about node status
-    TOTAL_INFO=$(curl -s ${CURL}${IDENTITY})
-
-    # if node is outdated > alarm
-    OUTDATED=$(echo ${TOTAL_INFO} | jq ".outdated" | tr -d '"')
-    if [[ "${OUTDATED}" == "true" ]]
-    then
-        SEND=1
-        VERSION=$(echo ${TOTAL_INFO} | jq ".mixnode.mix_node.version" | tr -d '"')
-        TEXT="_version >> ${VERSION}.\n_outdated > ${OUTDATED}."
-        __sendFunc
-    fi
-
-    # if status is not active > alarm
-    STATUS=$(echo ${TOTAL_INFO} | jq ".mixnode.status" | tr -d '"')
-    if [[ "${STATUS}" != "active" ]]
-    then
-        SEND=1
-        TEXT="_status >>> ${STATUS}."
-        __sendFunc
-    fi
-
-    # get info about stake
-    DELEGATION=$(echo $(echo "scale=2;$(echo ${TOTAL_INFO} | jq ".mixnode.total_delegation.amount" | tr -d '"')/${DENOM}" | bc))
-    SELF_STAKE=$(echo $(echo "scale=2;$(echo ${TOTAL_INFO} | jq ".mixnode.pledge_amount.amount" | tr -d '"')/${DENOM}" | bc))
-    TOTAL_STAKE=$(echo ${SELF_STAKE} + ${DELEGATION} | bc -l)
-    TEXT="stake >>>>> ${TOTAL_STAKE} ${TOKEN}."
-    __sendFunc
-
-    # get info about rewards
-    TOTAL_REWARDS=$(curl -s ${CURL}${IDENTITY}"/estimated_reward")
-
-    # get info about estimated rewards
-    ESTIMATED_REWARDS_H=$(echo $(echo "scale=3;$(echo ${TOTAL_REWARDS} | jq ".estimated_operator_reward" | tr -d '"')/${DENOM}" | bc))
-    ESTIMATED_REWARDS_M=$(echo ${ESTIMATED_REWARDS_H}*720 | bc -l)
-    if (( $(bc <<< "${ESTIMATED_REWARDS_H} < 1") )); then ESTIMATED_REWARDS_H="0${ESTIMATED_REWARDS_H}"; fi
-    if (( $(bc <<< "${ESTIMATED_REWARDS_M} < 1") )); then ESTIMATED_REWARDS_M="0${ESTIMATED_REWARDS_M}"; fi
-    TEXT="salary >>>> $ESTIMATED_REWARDS_H ${TOKEN}/h, $ESTIMATED_REWARDS_M ${TOKEN}/m."
-    __sendFunc
-
-    REWARDS=$(echo $(echo "scale=2;$(echo ${TOTAL_INFO} | jq ".mixnode.accumulated_rewards" | tr -d '"')/${DENOM}" | bc))
-    if (( $(bc <<< "${REWARDS} < 1") )); then REWARDS="0${REWARDS}"; fi
-    TEXT="unpaid >>>> ${REWARDS} ${TOKEN}."
-    __sendFunc
 
     if [[ ${SEND} == "1" ]]; then
        curl --header 'Content-Type: application/json' \
