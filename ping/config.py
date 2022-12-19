c2cad78944fe87d41532ef86d3f91a8c3e8c5b2e
cyberomanov <41644451+cyberomanov@users.noreply.github.com>
2022-12-19 16:55:28 +0400

    Create config.py

---
+++ ping/config.py
@@ -0,0 +1,11 @@
+SERVERS = {
+    "name1": "21.9.17.3",
+    "name2": "21.9.17.4",
+    "name3": "21.9.17.5",
+}
+
+BOT_API_TOKEN = "1703:AA-BB_CC"
+ALARM_CHAT_ID = "-2109"
+
+OUTPUT_LOG_PATH = "./log/ping.log"
+TIME_SLEEP_BETWEEN_PINGS_SEC = 60
